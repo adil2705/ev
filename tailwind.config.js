dbf347bff35c209d0bae489235b91cb9514b4249
adil2705 <ahmadadil2705@gmail.com>
2023-08-10 00:56:43 +0530

    commit message

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily: {
+        audiowide: ['Audiowide', 'sans'], // 'sans' fallback in case font isn't available
+      },
+    },
+  },
+  plugins: [],
+}
+
